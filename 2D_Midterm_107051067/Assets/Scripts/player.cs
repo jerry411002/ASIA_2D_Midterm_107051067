1b31b4434da7a582241624a489fc68d8bf2495f4
jerry411002 <71679892+jerry411002@users.noreply.github.com>
2020-11-10 21:09:59 +0800

    完成

--- 2D_Midterm_107051067/Assets/Scripts/player.cs
+++ 2D_Midterm_107051067/Assets/Scripts/player.cs
@@ -1,13 +1,25 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
+    public Text textcount;
+    public int count;
     public GameObject final;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送們了");
-        final.SetActive(true);
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+        if (collision.tag == "鑽石")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = "Gem:" + count;
+        }
     }
+
+
 }
